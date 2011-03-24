1a97760d13117efbabb9434a74596f4a7fd1fc3f
Kevin Horst <krhorst@gmail.com>
2011-03-23 21:03:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sketchmanager::Application.config.secret_token = '132e3d3d26cdf183bfa8ddb130e9cb9b753e57d778282988eff47a728a9c0c56cc574399f5af9f41d4c7645fc45e6fd5af425ec1696d6055ddcad10e5fa2b4e7'
