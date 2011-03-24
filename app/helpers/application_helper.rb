af11b70e342e01cd937231a986cd9f4bc229a42f
Kevin Horst <krhorst@gmail.com>
2011-03-23 23:34:44 -0500

    Added User Controller and multiple tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def title
+    
+  base_title = "Sketchmanager"
+  if @title.nil?
+    return base_title
+  else
+    return "#{base_title} | #{@title}"
+  end
+  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Sketchmanager::Application.routes.draw do
+  get "users/new"
+
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
+  match 'about' => 'pages#about'
+  
+  match 'contact' => 'pages#contact'
+  
+  match 'signup' => 'users#new'
+
+  root :to => "pages#home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +64,7 @@ Sketchmanager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
