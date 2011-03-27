d10485a270e41e42bd3b4f868ca162dc53ba878f
Kevin Horst <krhorst@gmail.com>
2011-03-27 14:04:36 -0500

    Completed User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "Sign Up"
   end
     
   def show
     @user = User.find(params[:id])
   end    
+  
+  def create
+    @user = User.create(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Sketchmanager"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'  
+    end  
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe UsersController do
-render_views
+
+  render_views
 
   describe "GET 'new'" do
     it "should be successful" do
@@ -33,4 +34,59 @@ render_views
     
   end
 
-end
+  describe "POST 'create" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => ""}
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create,:user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign Up")
+      end
+      
+      it "should render the 'new' page " do
+        post :create, :user => @attr
+        response.should render_template("new")
+      end
+    end
+
+    describe "success" do 
+
+      before(:each) do
+        @attr = { :name => "Kevin Horst", 
+                  :email => "krhorst@gmail.com", 
+                  :password => "passcode", 
+                  :password_confirmation => "passcode"
+                  }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the User 'show' page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should display a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to Sketchmanager/i
+      end
+
+    end
+  
+  end 
+
+end   
